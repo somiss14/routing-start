e6ad1b930a156329eb1e2f271d2ef7fc99928bdc
maciek@arch <mcyran1996@gmail.com>
2023-10-05 14:00:55 +0200

    lesson 150

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 import {AppRoutingModule} from "./app-routing.module";
 import {AuthService} from "./auth.service";
 import {AuthGuardService} from "./auth-guard.service";
+import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
 
 @NgModule({
   declarations: [
@@ -31,7 +32,7 @@ import {AuthGuardService} from "./auth-guard.service";
     FormsModule,
     AppRoutingModule
   ],
-  providers: [ServersService, AuthService, AuthGuardService],
+  providers: [ServersService, AuthService, AuthGuardService, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {
